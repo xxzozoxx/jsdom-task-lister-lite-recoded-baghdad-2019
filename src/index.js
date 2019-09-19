5a5d8fcbbcfc8cd38037693dcca2fc420b76db1c
zozo <zahraa.jalaa99@gmail.com>
2019-09-19 21:07:31 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    console.log('tasks');
 });
+let input = document.getElementById('new-task-description');
+let list = document.getElementById('tasks');
+
+function todo(event) {
+    event.preventDefault();
+    let inputVal = input.value;
+    let task = document.createElement('li');
+    task.innerText = inputVal;
+    list.appendChild(task);
+}
+document.onsubmit = todo;
